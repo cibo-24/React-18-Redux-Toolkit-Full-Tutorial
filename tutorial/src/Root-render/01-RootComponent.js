77a2f1f77802edf68ed37dfbe97dbb95e97c8f7e
cibo-24 <cibo.kenan@gmail.com>
2023-12-20 22:24:05 +0300

    working react

--- tutorial/src/Root-render/01-RootComponent.js
+++ tutorial/src/Root-render/01-RootComponent.js
@@ -1,12 +1,13 @@
+// index.js içerisinde render etmek istediklerimizi aşağıda ki gibi render edebiliriz.
+
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
 function Greeting() {
-  return <h2>My First Component: 'React'</h2>
+  return <h2>My First Component</h2>
 }
-
-
 
 // oluşturduğumuz fonksiyonu ReactDom ile manipüle edip createRoot'a eşitleyip
 // daha sonra render etmek üzere func içeri alıyoruz
