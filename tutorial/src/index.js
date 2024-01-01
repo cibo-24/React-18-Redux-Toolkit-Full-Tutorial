5428294e7fd2d2ffacca714a9792c05a3b3d3e87
cibo-24 <cibo.kenan@gmail.com>
2024-01-01 17:40:52 +0300

    object, props, arrays, key

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -6,36 +6,89 @@ import './index.css';
 //  return <h2 id='something' className='someValue'></h2>
 // }
 
-const author = "Jordan Moore";
-const title = "Interesting Facts For Curious Minds";
-const img = "https://images-na.ssl-images-amazon.com/images/I/71hwUY5ZmxL._AC_UL600_SR600,400_.jpg";
+const firstBook = [
+  {
+    author: "Jordan Moore",
+    title: "Interesting Facts For Curious Minds",
+    img: "https://images-na.ssl-images-amazon.com/images/I/71hwUY5ZmxL._AC_UL600_SR600,400_.jpg",
+  }
+]
+
+const secondBook = [
+  {
+    author: "James Clear",
+    title: "Atomic Habits",
+    img: "https://m.media-amazon.com/images/I/81YkqyaFVEL._SY522_.jpg",
+  }
+]
+
+
+
+// secondBook object'inin propertylerini ayırdık.
+const {author,title} = secondBook;
+console.log(author, title);
+
+
+const styleBook = {
+  size: 150,
+}
+
+const names = ["Amazon Best Seller"];
+const newNames = names.map((name) => {
+  if (name === "Amazon Best Seller" || name === "Best"){
+    return <h1>{name}</h1>
+  }else {
+    return <h2>Where're you doing?</h2>
+  } 
+  
+})
+
+
 
 
 const BookList = () => {
   return(
 
   <section className='bookList'>
-    <Book author={author} title={title} img={img}/>
-    <Book author={author} title={title} img={img}/>
+    <h2>{newNames}</h2>
+    <Book 
+    author={firstBook.author} 
+    title={firstBook.title} 
+    img={firstBook.img} 
+    size={styleBook.size}
+    />
+    <Book 
+    author={secondBook.author} 
+    title={secondBook.title} 
+    img={secondBook.img}
+    size={styleBook.size}
+    />
     
   </section>
   ) ;
 }
 
-const Book = (props) => {
+const Book = ({img, title, author, size, children}) => {
   // props, eğer üst component'te propsları verilmemişse boş obj döndürür.
   // ancak Booklist içerisinde ki Book Componentlerine prop atarsak, burada "props" adlı prop orada ki değerleri alacaktır.
   // aldığı verileri props.job, props.title vb okuyabiliriz.
   // bu propslar aracılığıyla componentler arası veri alışverişini ve haberleşmeyi sağlar.
+  // props'u propertylerine ayırdık. Ve onları ayrı ayrı kullandık.
 
-  console.log(props);
+  // children, Üst component eğer içerisine başka bir component almak istiyorsak bunu children props'unu vererek halledebiliriz.
+  
+
   return (
   <article className='book'>
     <img
-    src={props.img}
-    alt={props.title} />
-    <h2>{props.title}</h2>
-    <h4>{props.author}</h4>
+    src={img}
+    alt={title} />
+    <h2>{title}</h2>
+    <h4>{author}</h4>
+    <h4>{size}</h4>
+    <p>No matter your goals, Atomic Habits offers a proven framework for improving-every day. James Clear, one of the world's.</p>
+    <button>Click me</button>
+    {children}
     
     
   </article>
